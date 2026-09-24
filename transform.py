4a5018a25f496c1cfea26fd6542f1ba0ac5045dc
Chiranjeevi96 <chiran.gv96@gmail.com>
2026-09-24 19:26:15 +0530

    "updating docstrings"

--- transform.py
+++ transform.py
@@ -2,16 +2,20 @@ import pandas as pd
 from extract import extract_data
 
 def formatting(data):
+    """Function to formatting the messy data"""
     data['Region'] = data['Region'].str.strip().str.capitalize()
     data['Sales Rep'] = data['Sales Rep'].str.strip().str.capitalize()
     return data
 def dedupe(data):
+    """Function to delete the duplicate rows available in the dataframe"""
     data = data.drop_duplicates().reset_index(drop=True)
     return data
 def dropping(data):
+    """Function to dropping the unwanted rows"""
     data = data.dropna(subset=['Sales Rep']).reset_index(drop=True)
     return data
 def filling(data):
+    """Function to fill the missing values"""
     units_mean = data['Units Sold'].mean()
     revenue_mean = data['Revenue'].mean()
     data = data.fillna({'Units Sold': units_mean, 'Revenue': revenue_mean})
